ea6cdd908d47eb706cf8d15844745bcb38b64502
bonnerac <alex.c.bonner@gmail.com>
2020-02-25 20:32:14 -0600

    added speech bubbles, six characters to tiers page

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,28 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from "./pages/LandingPage"
-import LoginSignUp from "./pages/LoginOrSignup"
-import PersonalizedHomePage from "./pages/PersonalizedHomepage"
-import PrivacyPolicy from "./pages/PrivacyPolicy"
-import Purchase from "./pages/Purchase"
-import Sitemap from "./pages/Sitemap"
-import Tiers from "./pages/Tiers"
+import Home from "./pages/landingpage/"
+import LoginSignUp from "./pages/loginorsignup"
+import PersonalizedHomePage from "./pages/personalizedhomepage"
+import PrivacyPolicy from "./pages/privacypolicy"
+import Purchase from "./pages/purchase"
+
+import Tiers from "./pages/tiers"
 
 
 function App() {
   return (
     <Router>
       <div>
-        <Nav />
+
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/LoginOrSignUp" component={LoginSignUp} />
           <Route exact path="/Personalized" component={PersonalizedHomePage} />
           <Route exact path="/PrivacyPolicy" component={PrivacyPolicy} />
           <Route exact path="/Purchase" component={Purchase} />
-          <Route exact path="/Sitemap" component={Sitemap} />
+
           <Route exact path="/Tiers" component={Tiers} />
-          <Route component={NoMatch} />
+
         </Switch>
       </div>
     </Router>
--- client/src/pages/tiers/index.js
+++ client/src/pages/tiers/index.js
@@ -1,8 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
+import './layout.css'
+import './style.css'
+// import HeroCard from '../../components/HeroCard';
+// import data from '../../components/HeroCard/heroinfo.json';
 
 function Tiers() {
     return (
-        <div></div>
+        <div className="container" >
+            <div className="title">TITLE: CHOOSE YOUR TIER</div>
+            <div className="tier1"></div>
+            <div className="tier2"></div>
+            <div className="hero1 heroCard">
+                <div className="hoverImage hover1"></div>
+            </div>
+            <div className="hero2 heroCard">
+                <div className="hoverImage hover2"></div>
+            </div>
+            <div className="hero3 heroCard">
+                <div className="hoverImage hover3"></div>
+            </div>
+            <div className="hero4 heroCard">
+                <div className="hoverImage hover4"></div>
+            </div>
+            <div className="hero5 heroCard">
+                <div className="hoverImage hover5"></div>
+            </div>
+            <div className="hero6 heroCard">
+                <div className="hoverImage hover6"></div>
+            </div>
+        </div>
     )
 }
+
+
 export default Tiers;
+
+
+
+
+{/* <HeroCard shadow={element["shadow-image"]} gif={element["gif"]} key={"box-" + element.name} name={element.name} /> */ }
