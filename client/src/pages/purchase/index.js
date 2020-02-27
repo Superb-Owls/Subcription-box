b51c57f4ae12e18285be05f7607e02da60760fc8
Zeliha Gencel <zelihagencel@gmail.com>
2020-02-27 11:29:20 -0600

    progress with stripe

--- client/src/pages/purchase/index.js
+++ client/src/pages/purchase/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 
-function Purchase() {
+import CheckoutForm from '../../components/CheckoutForm';
+
+// Make sure to call `loadStripe` outside of a component’s render to avoid
+// recreating the `Stripe` object on every render.
+const stripePromise = loadStripe("pk_test_Pn2QWutVhjdDrcFaKWDoM5DJ00ZLEeus2G");
+
+function App() {
     return (
-        <div></div>
-    )
-}
-export default Purchase;
+        <Elements stripe={stripePromise}>
+            <CheckoutForm />
+        </Elements>
+    );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
