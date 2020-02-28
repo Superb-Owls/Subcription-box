c9ae73082636d089efe0f061710448972877967c
bonnerac <alex.c.bonner@gmail.com>
2020-02-28 15:13:14 -0600

    FE updates to landing page

---
+++ client/src/components/Testimonial/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './style.css'
+
+
+function Testimonial() {
+    return (
+        <>
+            <img className="test1" src="https://i.imgur.com/xsSgEhY.jpg"></img>
+            <img className="speech" src="https://i.imgur.com/eFybDAc.png?1"></img>
+            <p className="quote">I love having my <br /> favorite comics delivered <br /> to my door every month!</p>
+            <p className="name">Catherine Jones - Charleston, SC</p>
+
+        </>
+    )
+}
+export default Testimonial;
