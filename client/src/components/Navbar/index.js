4b9df6b57589ad3f61266e685dccbc8918539d71
bonnerac <alex.c.bonner@gmail.com>
2020-02-29 20:54:12 -0600

    Created navbar with sidebar for cart.

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -2,15 +2,37 @@ import React from "react";
 import './style.css'
 
 function Navbar() {
+
+    function openNav() {
+        document.getElementById("mySidebar").style.width = "250px";
+        document.getElementById("root").style.marginRight = "250px";
+    }
+    function closeNav() {
+        document.getElementById("mySidebar").style.width = "0";
+        document.getElementById("root").style.marginRight = "0";
+    }
     return (
-        <div className="navwrapper">
-            {/* <img src="https://i.imgur.com/BIFnTDQ.png">
-            </img> */}
-            test
-        </div>
+        <>
+            <div className="topnav">
+                <a className="active" href="/">
+                    HERO BOX
+                </a>
+                <a className="openbtn" onClick={openNav} style={{ float: "right" }}>
+                    <i className="fa fa-shopping-cart"></i>
 
+                </a>
+                <a href="login" style={{ float: "right" }}>LOGIN</a>
+                <a href="/tiers" style={{ float: "right" }}>SUBSCRIPTIONS</a>
+            </div>
+
+            <div id="mySidebar" className="sidebar">
+                <div className="closebtn" onClick={closeNav} href="javascript:void(0)">&times;</div>
+
+                <div>Your selections</div>
+                <div>Go to your cart</div>
+            </div>
+        </>
     )
-
 }
 
 export default Navbar;
