ea104d9c8070b303ba686e2047b76be27c7d4ed9
Zeliha Gencel <zelihagencel@gmail.com>
2020-03-03 12:24:55 -0600

    stripe works with new version of app

--- server.js
+++ server.js
@@ -7,6 +7,26 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 
+
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,16 +34,6 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
 }
-
-// app.get("/secret", async (req, res) => {
-//     console.log("HIT THE STRIPE SECRET!!!")
-//     const intent = await stripe.paymentIntents.create({
-//         amount: 1099,
-//         currency: 'usd',
-//         payment_method_types: ['card'],
-//     });// ... Fetch or create the PaymentIntent
-//     res.json({ client_secret: intent.client_secret })
-// });
 
 // Add routes, both API and view
 app.use(routes);
